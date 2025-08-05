3db9bcdd5711a8f77a704210526e2a864ebc3145
Jaya <akulajayateja4444@gmail.com>
2025-08-06 02:13:33 +0530

    I can now send an static html page as response.

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const { log } = require("console");
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-  console.log("Raw request", req);
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Hello, manual server\n");
+  // console.log("Raw request", req);
+  const requiredPath = req.url;
+
+  if (requiredPath === "/greet" && req.method === "GET") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "Hello from my server, result for greeting." })
+    );
+  } else if (requiredPath === "/time" && req.method === "GET") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Hello, the time is:",
+        time: new Date().toISOString(),
+      })
+    );
+  } else if (requiredPath === "/index.html" && req.method === "GET") {
+    const pathfile = path.join(__dirname, "static", "index.html");
+    fs.readFile(pathfile, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "content-type": "text/plain" });
+        res.end("Internal server error");
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(data);
+      }
+    });
+    // res.writeHead(200, { "content-type": "text/html" });
+    // res.end("<h1>HTML FILE YAY!</h1>");
+  } else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.end(JSON.stringify({ error: "Not found." }));
+  }
 });
 
-server.listen(8080, () => {
-  console.log("Server running at http://localhost:8080");
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
 });
